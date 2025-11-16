e3b3e33a8c060f22ea5a6de3a7e818547d4a3ea5
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 20:57:49 +0530

    Update 6-4.java

--- java exp/Exp6/6-4.java
+++ java exp/Exp6/6-4.java
@@ -1,1 +1,26 @@
+interface A{
+void display1();
+}
+
+interface B extends A{
+void display2(); 
+} 
+
+class C implements B{
+public void display1(){
+System.out.println("display function of interface A");
+}
+
+public void display2(){
+System.out.println("display function of interface B");
+}
+}
+
+class driverin{
+public static void  main(String[] args){
+C obj = new C();
+obj.display1();
+obj.display2();
+System.out.println("URVI NARANG 24CSU226");
+}}
 
