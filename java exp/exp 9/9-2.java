4528e2cd912ec49aa3ff4cddcebcd90a31682c07
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 17:23:00 +0530

    Update 9-2.java

--- java exp/exp 9/9-2.java
+++ java exp/exp 9/9-2.java
@@ -1,1 +1,21 @@
+import java.io.File;
+class ChangeFilePermissions {
+public static void main(String[] args) {
+File file = new File("check.txt"); 
+System.out.println("Can Read: " + file.canRead());
+System.out.println("Can Write: " + file.canWrite());
+if (file.exists()) {
+boolean result = file.setWritable(false);
+if (result) {
+System.out.println("File permission changed"); }
+else {
+System.out.println("Failed to change file permission.");  }
+}
+else {
+System.out.println("File does not exist.");  }
+
+System.out.println("Can Read: " + file.canRead());
+System.out.println("Can Write: " + file.canWrite());
+System.out.println("URVI NARANG 24CSU226");
+}}
 
