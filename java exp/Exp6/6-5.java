f0e0253e49768ce3f4e5b8247a76b59024595299
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 20:58:28 +0530

    Update 6-5.java

--- java exp/Exp6/6-5.java
+++ java exp/Exp6/6-5.java
@@ -1,1 +1,38 @@
+interface one{
+void display1();}
 
+interface two{
+void display2();}
+
+interface three extends one, two{
+void display3();}
+
+class real implements three{
+public void display4(){
+System.out.println("display function of class");}
+public void display1(){
+System.out.println("Display function of interface 1");}
+public void display2(){
+System.out.println("Display function of interface 2");}
+public void display3(){
+System.out.println("Display function of interface 3");}
+}
+
+class driverinterface{
+public static void main(String args[]){
+real obj = new real();
+obj.display1();
+obj.display2();
+obj.display3();
+obj.display4();
+System.out.println("URVI NARANG 24CSU226");
+
+}}
+
+
+
+
+
+
+
+  
