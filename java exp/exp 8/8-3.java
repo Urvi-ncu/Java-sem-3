c5e5d0943c32dd7da21930c838d10addea441f53
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 17:12:35 +0530

    Update 8-3.java

--- java exp/exp 8/8-3.java
+++ java exp/exp 8/8-3.java
@@ -1,1 +1,15 @@
+import java.lang.Throwable;
+class ExceptionExample{
+public static void main(String args[]){
+try{
+throw new Exception("Exception occurred");
+}
+catch(Exception e){
+System.out.println("Caught exception "+e);
+}
+finally{
+System.out.println("Finally block");
+System.out.println("URVI NARANG 24CSU226");
+}
+}}
 
