f5bdcf3b6c8ec6058539375a37bfdae680770823
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 21:02:37 +0530

    Update 6-8.java

--- java exp/Exp6/6-8.java
+++ java exp/Exp6/6-8.java
@@ -1,1 +1,35 @@
+abstract class Shape
+{
+abstract void RectangleArea(double length,double breadth);
+abstract void SquareArea(double side);
+abstract void CircleArea(double radius);
+}
+
+class Area extends Shape
+{
+void RectangleArea(double length,double breadth)
+{
+System.out.println("Area of Rectangle: "+(length*breadth));
+}
+void SquareArea(double side)
+{
+System.out.println("Area of Square: "+(side*side));
+}
+void CircleArea(double radius)
+{
+System.out.println("Area of Circle: "+(3.14*radius*radius));
+}
+}
+
+class mainshape
+{
+public static void main(String[] args)
+{
+Area a=new Area();
+a.RectangleArea(5,10);
+a.SquareArea(4);
+a.CircleArea(3);
+System.out.println("URVI NARANG 24CSU226");
+}
+}
 
