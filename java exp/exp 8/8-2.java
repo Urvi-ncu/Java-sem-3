36c01971503ff2d1491d4b65d5ab42dd0f3008b5
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 17:11:01 +0530

    Update 8-2.java

--- java exp/exp 8/8-2.java
+++ java exp/exp 8/8-2.java
@@ -1,1 +1,21 @@
+import java.lang.Throwable;
+class A extends Exception {
+String message;
+public A(String message){
+this.message=message;
+}
+public void display(){
+System.out.println("Stored Message: "+message);
+}
+}
+
+public class CustomException{
+public static void main(String args[]){
+try{
+throw new A("This is my custom exception message"); }
+catch(A e){
+e.display(); }
+finally{
+System.out.println("URVI NARANG 24CSU226"); }
+}}
 
