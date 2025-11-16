bcea74b1cacee158f35f024db0d43b279046fd94
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 20:56:12 +0530

    Update 6-2.java

--- java exp/Exp6/6-2.java
+++ java exp/Exp6/6-2.java
@@ -1,1 +1,39 @@
+abstract class Bank
+{
+abstract void getBalance();}
+
+class BankA extends Bank
+{
+void getBalance()
+{
+System.out.println("Balance in Bank A: $100");}
+}
+
+class BankB extends Bank
+{
+void getBalance()
+{
+System.out.println("Balance in Bank B: $150");}
+}
+
+class BankC extends Bank
+{
+void getBalance()
+{
+System.out.println("Balance in Bank C: $200");}
+}
+
+class mainbank
+{
+public static void main(String[] args)
+{
+BankA a=new BankA();
+BankB b=new BankB();
+BankC c=new BankC();
+a.getBalance();
+b.getBalance();
+c.getBalance();
+System.out.println("URVI NARANG 24CSU226");
+}
+}
 
