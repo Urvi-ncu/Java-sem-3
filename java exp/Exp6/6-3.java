76646a31bfb836874c36ea3e8d80222594a8ba2b
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 20:56:57 +0530

    Update 6-3.java

--- java exp/Exp6/6-3.java
+++ java exp/Exp6/6-3.java
@@ -1,1 +1,16 @@
+public class circle implements geometricobject{
+double radius = 1.0;
 
+circle(double radius){
+this.radius = radius;}
+
+public double getperimeter(){
+return 2*3.14*radius;}
+
+public double getarea(){
+return 3.14*radius*radius;}
+
+public double getradius(){
+return radius;}
+
+}
