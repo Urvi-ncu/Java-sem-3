eb2ac55685686066418ec9812e53d02590fe3652
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 21:08:27 +0530

    Update 7-2.java

--- java exp/exp 7/7-2.java
+++ java exp/exp 7/7-2.java
@@ -1,1 +1,8 @@
+package exercise.java;
 
+public class packagedemodriver{
+public static void main(String args[]){
+packagedemo obj = new packagedemo();
+obj.display();
+System.out.println("URVI NARANG 24CSU226");
+}}
