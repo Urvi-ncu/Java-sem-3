385b145b0a792c8f369ae49f955137a688705174
Urvi-ncu <24csu226@ncuindia.edu>
2025-11-16 21:05:46 +0530

    Update 6-10.java

--- java exp/Exp6/6-10.java
+++ java exp/Exp6/6-10.java
@@ -1,1 +1,9 @@
+public class resizeablecircle extends circle implements resizeable{
+public resizeablecircle(double radius){
+super(radius);}
 
+public void resize(int percent){
+radius = radius *percent/100;}
+
+
+}
